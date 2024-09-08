fec288c3af82cc233bdb9c7919cb0a7072f9b090
Sparsh Amitbhai Ladani <sp835101@dal.ca>
2024-09-08 19:14:13 -0300

    Integrated Github API to fetch all projects from my github repository

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,42 +1,47 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import sample from "../../Assets/Projects/sample movie.png";
-import events from "../../Assets/Projects/image.png";
+import gitimage from "../../Assets/Projects/gitimage.png";
+import axios from "axios";
 
 function Projects() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const repositories = await axios.get("https://api.github.com/users/sparshladani/repos");
+        setProjects(repositories.data);
+      } catch (error) {
+        console.error("Error fetching data from repository");
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <Container fluid className="project-section">
       <Particle />
       <Container>
         <h1 className="project-heading">
-          My Recent <strong className="purple">Works </strong>
+          My Recent <strong className="purple">Works</strong>
         </h1>
         <p style={{ color: "white" }}>
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={events}
-              isBlog={false}
-              title="Event Mangement System"
-              description="Organize personal events, post your event and invite new people to the event by providing the description of the event. This web application was made using php and also uses login and signup process for a user in order to post events."
-              ghLink="https://github.com/SparshLadani/Event-Management-System"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={sample}
-              isBlog={false}
-              title="Movie Reviews"
-              description="Build a movie review web application like imdb that allows a user to login and then post a review for more than 20 thousand movies. This project is build using modern day technologies like MongoDB, ExpressJS, ReactJS and NodeJS. Furthermore, the sample data in this project is retrieved from MongoDB Atlas."
-              ghLink="https://github.com/SparshLadani/Mern-Movie-Reviews"
-              demoLink="https://movie-reviews-app.netlify.app/"
-            />
-          </Col>
+          {projects.map((project, index) => (
+            <Col key={index} md={4} className="project-card" style={{ marginBottom: "20px" }}>
+              <ProjectCard
+                imgPath={gitimage}
+                isBlog={false}
+                title={project.name}
+                description={project.description}
+                ghLink={project.html_url}
+              />
+            </Col>
+          ))}
         </Row>
       </Container>
     </Container>
